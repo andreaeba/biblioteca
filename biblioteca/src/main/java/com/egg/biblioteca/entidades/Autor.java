1f07facf834c16a084524ac20b864de284f04172
Andrea Bermúdez <andreabusb@gmail.com>
2025-02-26 00:17:42 -0300

    first commit

---
+++ biblioteca/src/main/java/com/egg/biblioteca/entidades/Autor.java
@@ -0,0 +1,41 @@
+package com.egg.biblioteca.entidades;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+public class Autor {
+
+    @Id
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name = "uuid", strategy = "uuid2")
+    private String id;
+
+    private String nombre;
+
+    public Autor(String id, String nombre) {
+        this.id = id;
+        this.nombre = nombre;
+    }
+
+    public Autor() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+}
---
+++ biblioteca/src/main/java/com/egg/biblioteca/servicios/EditorialServicio.java
@@ -0,0 +1,24 @@
+package com.egg.biblioteca.servicios;
+
+import com.egg.biblioteca.entidades.Editorial;
+import com.egg.biblioteca.repositorios.EditorialRepositorio;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EditorialServicio {
+
+    @Autowired
+    private EditorialRepositorio editorialRepositorio;
+
+    private void crearEditorial(String nombre){
+
+        Editorial editorial = new Editorial();
+
+        editorial.setNombre(nombre);
+
+        editorialRepositorio.save(editorial);
+    }
+
+
+}
